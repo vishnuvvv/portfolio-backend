2f09944482b9aa0c364e8428e5da42fb269944cf
vishnu <vishnusathyanathan@gmail.com>
2023-09-29 10:57:54 +0530

    backend server started

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
 
+dotenv.config();
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 5000;
+const DB_URI = process.env.DB;
 
-app.listen(5000,()=>{
-    console.log("server listening port 5000")
-})
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+const startServer = async () => {
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+
+    // Start the Express server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    // Handle server errors
+    app.on("error", (err) => {
+      console.error("Error starting the server:", err);
+    });
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
+};
+
+startServer();
